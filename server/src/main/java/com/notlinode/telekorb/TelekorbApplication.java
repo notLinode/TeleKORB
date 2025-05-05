2c1b44174b4b15e58150df137d665cd897d934ba
Iskenderov Andrei <linode@duck.com>
2025-05-05 14:56:39 +0300

    Initial server commit

---
+++ server/src/main/java/com/notlinode/telekorb/TelekorbApplication.java
@@ -0,0 +1,13 @@
+package com.notlinode.telekorb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelekorbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelekorbApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/notlinode/telekorb/TelekorbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notlinode.telekorb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelekorbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
